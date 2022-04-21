55c0fe0281f138d94211523b727e731f68087bce
Umidjon <mrumidjon2020@gmail.com>
2022-04-21 23:55:35 +0500

    count letter - Bu funksiya wordni ichida nechta letter bor ekanligni returni qiladi.

---
+++ python/14-Polimorfizm-Paket-modul/paket1/modul2.py
@@ -0,0 +1,7 @@
+def count_letter(word, letter):
+    "Bu funksiya wordni ichida nechta letter bor ekanligni returni qiladi."
+    k = 0
+    for i in word:
+        if word == letter:
+            k += 1
+    return k
