5b3768d07a977a486a0ea4f2965f444ab5f0c3d2
Umidjon <mrumidjon2020@gmail.com>
2022-04-21 23:59:01 +0500

    modul 1

--- python/14-Polimorfizm-Paket-modul/paket1/modul2.py
+++ python/14-Polimorfizm-Paket-modul/paket1/modul2.py
@@ -2,6 +2,6 @@ def count_letter(word, letter):
     "Bu funksiya wordni ichida nechta letter bor ekanligni returni qiladi."
     k = 0
     for i in word:
-        if word == letter:
+        if i == letter:
             k += 1
     return k
