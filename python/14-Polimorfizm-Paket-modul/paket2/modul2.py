210e72e3fc2b3fa4b1b5333802f4ab1daade52a1
Umidjon <mrumidjon2020@gmail.com>
2022-04-22 00:09:43 +0500

    maxelem and minelem func

---
+++ python/14-Polimorfizm-Paket-modul/paket2/modul2.py
@@ -0,0 +1,8 @@
+def max_elem(myList):
+    """myListni ichidan maximumni topib returni qiladi."""
+    return max(myList)
+
+
+def min_elem(myList):
+    """myListni ichidan minimumni topib returni qiladi."""
+    return min(myList)
